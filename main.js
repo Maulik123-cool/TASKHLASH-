ba193c38b15b9b27a8cd9c70061acb99579ca1b0
Maulik123-cool <daga.niti84@gmail.com>
2025-07-14 11:50:42 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+let currentPoints = 200;
+
+function startBattle() {
+  alert("Battle Started! Your points: " + currentPoints);
+}
+
+function viewDetails() {
+  alert("Chris vs Bob: 60 vs 40\nHealth: 🔋🔋🔋");
+}
